91b501028c445015235e8654c65e902e1058d564
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-04-26 19:07:10 -0300

    Requisito 03, implementado botão do carrinho e rota para o cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
+          <Route exact path="/cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
