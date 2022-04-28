e4f97678be6b96a4b33381758281a98dbf9ad29b
Daniel <ohtosttnz@gmail.com>
2022-04-28 02:57:40 -0300

    feat: Implementa página do Carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
 
 class Cart extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cartItems: [],
+    };
+  }
+
+  componentDidMount = () => {
+    if (!JSON.parse(localStorage.getItem('cartItems'))) {
+      localStorage.setItem('cartItems', JSON.stringify([]));
+    }
+    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
+    this.setState({
+      cartItems,
+    });
+  }
+
+  getProductQuantity(id) {
+    const { cartItems } = this.state;
+    const numOfItems = cartItems
+      .filter((product) => id === product.id).length;
+    return numOfItems;
+  }
+
   render() {
+    const { cartItems } = this.state;
     return (
       <div>
-        <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
-        {/* Daniel esteve aqui */}
+        {
+          !cartItems
+            ? (
+              <h2 data-testid="shopping-cart-empty-message">
+                Seu carrinho está vazio
+              </h2>
+            )
+            : cartItems
+              .map(({ id, thumbnail, title, price }) => (
+                <div key={ id }>
+                  <h1 data-testid="shopping-cart-product-name">
+                    {title}
+                  </h1>
+                  <img
+                    src={ thumbnail }
+                    alt={ title }
+                  />
+                  <p>{`R$ ${price}`}</p>
+                  <span>Quantidade: </span>
+                  <span data-testid="shopping-cart-product-quantity">
+                    { this.getProductQuantity(id) }
+                  </span>
+                </div>
+              ))
+        }
       </div>
     );
   }
